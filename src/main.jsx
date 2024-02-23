f12163dab91538c76c0fbc3551002b67ead8f657
leobewater <mmbee888@gmail.com>
2024-02-23 18:11:59 -0500

    fix css

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./global.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 import { WithoutQuery } from "./pages/WithoutQuery.jsx";
 import { WithQuery } from "./pages/WithQuery.jsx";
 
